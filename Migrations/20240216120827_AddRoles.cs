d6cc8472fcc0eddc337780e3f7ae2577e00b6411
Brett Drake <recordstuff@hotmail.com>
2024-02-16 06:29:34 -0600

    add Roles as a migration.  first version of enforcing db constraints.

---
+++ Migrations/20240216120827_AddRoles.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace bretts_services.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "RoleID", "Name", "RoleGuid" },
+                values: new object[,]
+                {
+                    { 1L, "Admin", new Guid("cdf2beff-ea73-4d8b-9fe8-33818e52776f") },
+                    { 2L, "User", new Guid("111224ad-f6a4-4ca1-ade2-2e6ab407d8e8") }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "RoleID",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "RoleID",
+                keyValue: 2L);
+        }
+    }
+}
--- Models/EF/BrettsAppContext.cs
+++ Models/EF/BrettsAppContext.cs
@@ -1,4 +1,6 @@
-﻿namespace bretts_services.Models.EF;
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace bretts_services.Models.EF;
 
 public partial class BrettsAppContext : DbContext
 {
@@ -16,6 +18,11 @@ public partial class BrettsAppContext : DbContext
         {
             e.Property(r => r.RoleGuid)
                 .HasDefaultValueSql("NEWID()");
+
+            e.HasData(
+                new Role { RoleID = 1, Name = "Admin", RoleGuid = new Guid("cdf2beff-ea73-4d8b-9fe8-33818e52776f") },
+                new Role { RoleID = 2, Name = "User", RoleGuid = new Guid("111224ad-f6a4-4ca1-ade2-2e6ab407d8e8") }
+            );
         });
 
 
@@ -29,5 +36,47 @@ public partial class BrettsAppContext : DbContext
         });
 
         base.OnModelCreating(modelBuilder);
+    }
+
+    private void ValidateChanges()
+    {
+        foreach (var entity in ChangeTracker.Entries())
+        {
+            if (entity.Entity is User user)
+            {
+                if (entity.State == EntityState.Added 
+                 || entity.State == EntityState.Modified)
+                {
+                    if (!user.Roles.Any())
+                    {
+                        throw new NotSupportedException("User must have at least one Role.");
+                    }
+                }
+            }
+        }
+    }
+
+    public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+    {
+        ValidateChanges();
+
+        return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+    }
+
+    public override Task<int> SaveChangesAsync(System.Threading.CancellationToken cancellationToken = default)
+    {
+        return SaveChangesAsync(cancellationToken);
+    }
+
+    public override int SaveChanges(bool acceptAllChangesOnSuccess)
+    {
+        return base.SaveChanges(acceptAllChangesOnSuccess);
+    }
+
+    public override int SaveChanges()
+    {
+        ValidateChanges();
+
+        return SaveChanges(true);
     }
 }
