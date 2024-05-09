db26b20cd80ddd05b8ed1373860e24dbc49eae03
Brett Drake <recordstuff@hotmail.com>
2024-05-09 05:33:08 -0500

    add log migration manually

--- Migrations/20240509102921_Logs.Designer.cs
+++ Migrations/20240509102921_Logs.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using bretts_services.Models.Entities;
 
@@ -11,9 +12,11 @@ using bretts_services.Models.Entities;
 namespace bretts_services.Migrations
 {
     [DbContext(typeof(BrettsAppContext))]
-    partial class BrettsAppContextModelSnapshot : ModelSnapshot
+    [Migration("20240509102921_Logs")]
+    partial class Logs
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
